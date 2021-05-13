2b405e39bf1f62c3a8d03f35ae343e9a0a828a30
mrj <376531690@qq.com>
2021-05-13 20:56:16 +0800

    feat:添加修改石子游戏解法

---
+++ dynamic_programming/practice/stone-game/stone_game.go
@@ -0,0 +1,46 @@
+package stonegame
+
+// 石子游戏改：石头堆数改为任意整数，石子总数也改为任意总数
+func stoneGame2(piles []int) bool {
+	n := len(piles)
+	// 定义dp数组,i和j表示piles的下标，k两个值，0表示先手获取的最大值、1表示后手获取的最大值
+	dp := make([][][]int, n)
+	for i := 0; i < n; i++ {
+		dp[i] = make([][]int, n)
+		for j := 0; j < n; j++ {
+			dp[i][j] = make([]int, 2)
+			if i == j {
+				dp[i][j][0] = piles[i]
+				dp[i][j][1] = 0
+			}
+		}
+	}
+	//右对角线遍历
+	for i, j := 0, 1; j < n && j > i; {
+		left := dp[i+1][j][1] + piles[i]
+		right := dp[i][j-1][1] + piles[j]
+		if left > right {
+			dp[i][j][0] = left
+			dp[i][j][1] = dp[i+1][j][0]
+		} else {
+			dp[i][j][0] = right
+			dp[i][j][1] = dp[i][j-1][0]
+		}
+		if j == n-1 && i != 0 {
+			j = j - i + 1
+			i = 0
+		} else {
+			i++
+			j++
+		}
+	}
+
+	return dp[0][n-1][0] > dp[0][n-1][1]
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
---
+++ dynamic_programming/practice/stone-game/stone_game_test.go
@@ -0,0 +1,12 @@
+package stonegame
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestStoneGame2(t *testing.T) {
+	assert.Equal(t, true, stoneGame2([]int{3, 9, 1, 2}))
+	assert.Equal(t, false, stoneGame2([]int{1, 100, 2}))
+}
