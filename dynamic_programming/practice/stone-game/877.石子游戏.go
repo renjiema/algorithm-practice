df9b1ad70924ee5123d821d3918b1732a873863c
mrj <376531690@qq.com>
2021-05-13 19:39:39 +0800

    feat:添加石子游戏解法

---
+++ dynamic_programming/practice/stone-game/877.石子游戏.go
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=877 lang=golang
+ *
+ * [877] 石子游戏
+ */
+package stonegame
+
+// https://leetcode-cn.com/problems/stone-game
+// 解题分析：因为石子的总数是奇数，所以没有平局，
+// 因此我们按照索引将石头堆分为奇数堆和偶数堆，总会有一堆更大，
+// 因此先手的人总可以选择较大的堆。
+// @lc code=start
+func stoneGame(piles []int) bool {
+	return true
+}
+
+// @lc code=end
