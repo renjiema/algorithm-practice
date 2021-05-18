43aaccd34f9aec26f118e4999040e233aa33f322
mrj <376531690@qq.com>
2021-05-18 18:42:32 +0800

    feat:添加股票买卖的最佳时机iii动态规划解法

---
+++ dynamic_programming/practice/best_time_to_buy_and_sell_stock/123.买卖股票的最佳时机-iii.go
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=123 lang=golang
+ *
+ * [123] 买卖股票的最佳时机 III
+ */
+
+// @lc code=start
+func maxProfit(prices []int) int {
+	// 动态规划解法
+	dp := make([][][]int, n+1)
+	for i := 0; i <= n; i++ {
+		dp[i] = make([][]int, 3)
+		for j := 0; j <= 2; j++ {
+			dp[i][j] = make([]int, 2)
+		}
+	}
+	// base case
+	for j := 0; j <= 2; j++ {
+		dp[0][j][1] = math.MinInt32
+	}
+	for i := 1; i <= len(prices); i++ {
+		for j := 2; j >= 1; j-- {
+			// 第i天未持股：第i-1天未持股，或第i天卖出
+			dp[i][j][0] = max(dp[i-1][j][0], dp[i-1][j][1]+prices[i-1])
+			// 第i天持股：第i-1天持股，或第i天买入
+			dp[i][j][1] = max(dp[i-1][j][1], dp[i-1][j-1][0]-prices[i-1])
+		}
+	}
+	return dp[n][2][0]
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+// @lc code=end
+
