627c75b1dcf17823ad2b10923eff46b097d6746a
mrj <376531690@qq.com>
2021-05-18 18:38:43 +0800

    feat:添加股票买卖的最佳时机简单解法

--- dynamic_programming/practice/best_time_to_buy_and_sell_stock/121.买卖股票的最佳时机.go
+++ dynamic_programming/practice/best_time_to_buy_and_sell_stock/121.买卖股票的最佳时机.go
@@ -5,15 +5,40 @@
  */
 package besttimetobuyandsellstock
 
+import "math"
+
 // @lc code=start
 func maxProfit(prices []int) int {
 	res := 0
+	minVal := math.MaxInt32
 	for i := 0; i < len(prices); i++ {
-		for j := i + 1; j < len(prices); j++ {
-			res = max(res, prices[j]-prices[i])
+		if prices[i] < minVal {
+			minVal = prices[i]
+		} else if prices[i]-minVal > res {
+			res = prices[i] - minVal
 		}
 	}
+
 	return res
+}
+
+// @lc code=end
+
+func maxProfit2(prices []int) int {
+	n := len(prices)
+	dp := make([][]int, n+1)
+	for i := 0; i <= n; i++ {
+		dp[i] = make([]int, 2)
+	}
+	dp[0][1] = math.MinInt32
+	for i := 1; i <= n; i++ {
+		// 第i天未持股：第i-1天未持股，或第i天卖出
+		dp[i][0] = max(dp[i-1][0], dp[i-1][1]+prices[i-1])
+		// 第i天持股：第i-1天持股，或第i天买入
+		dp[i][1] = max(dp[i-1][1], -prices[i-1])
+	}
+
+	return dp[n][0]
 }
 func max(a, b int) int {
 	if a > b {
@@ -21,5 +46,3 @@ func max(a, b int) int {
 	}
 	return b
 }
-
-// @lc code=end
